6138ba456e7466ca0d2bbb4ee6e8ce1e309e32a0
Kangcheng Hou <kangchenghou@gmail.com>
2021-11-17 19:09:40 -0800

    update code

--- admix_genet_cor/locus.py
+++ admix_genet_cor/locus.py
@@ -6,6 +6,9 @@ import statsmodels.api as sm
 from tqdm import tqdm
 import admix
 import pandas as pd
+import dapgen
+from .utils import simulate_quant_pheno
+
 
 def calc_cov(m1, m2):
     # m1: (n_snp1, n_indiv)
@@ -16,10 +19,11 @@ def calc_cov(m1, m2):
     n_indiv = m1.shape[1]
     return np.dot(m1, m2.T) / n_indiv
 
+
 def calc_apa_cov(apa, m):
     # apa: (n_snp, n_indiv, 2)
     # m: (n_indiv, )
-    
+
     n_snp, n_indiv = apa.shape[0:2]
     assert apa.shape[1] == m.shape[0]
     # center apa
@@ -33,11 +37,12 @@ def calc_apa_cov(apa, m):
         cov_inv = np.linalg.inv(np.cov(apa[snp_i, :, :], rowvar=False))
         # expectation and variance
         ex = cov_inv @ apa[snp_i, :, :].T @ m
-#         z_list[snp_i] = transform_vec.T @ ex
+        #         z_list[snp_i] = transform_vec.T @ ex
         z_list[snp_i] = (
             transform_vec.T @ ex / np.sqrt(transform_vec.T @ cov_inv @ transform_vec)
         )
     return z_list
+
 
 def test_snp_het(apa, y, cov=None):
     """
@@ -51,7 +56,7 @@ def test_snp_het(apa, y, cov=None):
         design = sm.add_constant(np.hstack([apa, cov]))
     else:
         design = sm.add_constant(apa)
-        
+
     model = sm.OLS(y, design).fit()
 
     A = np.zeros([1, len(model.params)])
@@ -70,39 +75,109 @@ def marginal_het(geno, lanc, y, cov=None):
     """
     apa = admix.data.allele_per_anc(geno, lanc, n_anc=2).compute()
     n_snp, n_indiv, n_anc = apa.shape
-    
+
     if cov is None:
         design = np.ones((n_indiv, 1))
     else:
         design = np.hstack([np.ones((n_indiv, 1)), cov])
     design = np.hstack([design, np.zeros((n_indiv, n_anc))])
-    
+
     A = np.zeros([1, design.shape[1]])
     A[0, 1] = 1
     A[0, 2] = -1
-    
-    df_rls = {
-        "het_pval": [],
-        "coef1": [],
-        "se1": [],
-        "coef2": [],
-        "se2": []
-    }
+
+    df_rls = {"het_pval": [], "coef1": [], "se1": [], "coef2": [], "se2": []}
     pvals = np.zeros(n_snp)
 
     for snp_i in tqdm(range(n_snp)):
         design[:, -2:] = apa[snp_i, :, :]
         model = sm.OLS(y, design).fit()
-        df_rls['het_pval'].append(model.f_test(A).pvalue.item())
+        df_rls["het_pval"].append(model.f_test(A).pvalue.item())
         for anc_i in range(2):
             df_rls[f"coef{anc_i + 1 }"].append(model.params[anc_i + 1])
             df_rls[f"se{anc_i + 1}"].append(model.bse[anc_i + 1])
-        
 
     return pd.DataFrame(df_rls)
-    
+
+
 def test_snp_assoc(apa, y, cov):
     assert apa.shape[1] == 2
     design = sm.add_constant(np.hstack([apa.sum(axis=1)[:, np.newaxis], cov]))
     model = sm.OLS(y, design).fit()
     return model.pvalues[1], model
+
+
+def simulate_hetero_assoc(
+    pfile: str,
+    hsq: float,
+    causal_snp: str,
+    region_window: int = 50,
+    n_top_snp: int = 10,
+):
+    """
+    Simulate phenotype based on a region around one causal SNP
+    perform association testing for all the SNPs in the region
+
+    Parameters
+    ----------
+
+    pfile: pfile
+    hsq: heritability of the causal SNPs.
+    causal_snp: index of the causal SNP
+    region_window: window size in kb
+    n_top_snp: top associated SNPs for evaluating heterogeneity
+    """
+    # format data
+    np.random.seed(42)
+    geno, df_snp, df_indiv = dapgen.read_pfile(pfile, phase=True, snp_chunk=1024)
+    lanc = admix.io.read_lanc(pfile + ".lanc", snp_chunk=1024)
+    df_snp_info = pd.read_csv(pfile + ".snp_info", sep="\t").set_index("SNP")
+    assert np.all(df_snp_info.index == df_snp.index.values)
+    df_snp = pd.merge(df_snp, df_snp_info, left_index=True, right_index=True)
+
+    # determine subset of SNPs
+    causal_idx = df_snp.index.get_loc(causal_snp)
+    causal_pos = df_snp.loc[causal_snp, "POS"]
+
+    region_snp_idx = np.where(
+        (causal_pos - region_window * 1e3 <= df_snp.POS)
+        & (df_snp.POS < causal_pos + region_window * 1e3)
+    )[0]
+
+    df_tmp = df_snp.iloc[region_snp_idx]
+    # filter 0.001 < EUR_FREQ < 0.999 and 0.001 < AFR_FREQ < 0.999
+    region_snp_idx = region_snp_idx[
+        df_tmp.EUR_FREQ.between(0.001, 0.999) & df_tmp.AFR_FREQ.between(0.001, 0.999)
+    ]
+    print(len(region_snp_idx))
+
+    geno = geno[region_snp_idx, :, :]
+    lanc = lanc[region_snp_idx, :, :]
+    df_snp = df_snp.iloc[region_snp_idx, :]
+
+    n_eff_snp = df_snp.shape[0]
+
+    causal_idx = df_snp.index.get_loc(causal_snp)
+
+    # simulate phenotype
+    beta = np.zeros((n_eff_snp, 2, 1))  # (n_snp, n_anc, n_sim)
+    beta[causal_idx, :, :] = 1.0
+
+    sim = simulate_quant_pheno(geno=geno, lanc=lanc, hsq=hsq, beta=beta, n_sim=1)
+    pheno = sim["pheno"].flatten()
+
+    df_rls = marginal_het(
+        geno=geno,
+        lanc=lanc,
+        y=pheno,
+    )
+
+    df_rls["assoc_pval"] = admix.assoc.marginal_fast(
+        geno=geno, lanc=lanc, pheno=pheno, cov=None
+    )
+    df_rls["causal_snp"] = causal_snp
+    df_rls.index = df_snp.index
+    df_rls.index.name = "test_snp"
+    df_rls = df_rls.sort_values("assoc_pval").iloc[0:n_top_snp]
+    df_rls["assoc_pval_rank"] = np.arange(n_top_snp)
+    return df_rls
--- admix_genet_cor/utils.py
+++ admix_genet_cor/utils.py
@@ -1,5 +1,4 @@
 import numpy as np
-import allel
 import xarray as xr
 import pandas as pd
 from tqdm import tqdm
@@ -10,6 +9,252 @@ from os.path import join
 from typing import List
 import fire
 import statsmodels.api as sm
+import dask.array as da
+
+
+def af_per_anc(geno, lanc, n_anc=2) -> np.ndarray:
+    """
+    Calculate allele frequency per ancestry
+
+    If at one particular SNP locus, no SNP from one particular ancestry can be found
+    the corresponding entries will be filled with np.NaN.
+
+    Parameters
+    ----------
+    dset: xr.Dataset
+        Containing geno, lanc, n_anc
+
+    Returns
+    -------
+    List[np.ndarray]
+        `n_anc` length list of allele frequencies.
+    """
+    assert np.all(geno.shape == lanc.shape)
+    n_snp = geno.shape[0]
+    af = np.zeros((n_snp, n_anc))
+
+    snp_chunks = geno.chunks[0]
+    indices = np.insert(np.cumsum(snp_chunks), 0, 0)
+
+    for i in range(len(indices) - 1):
+        start, stop = indices[i], indices[i + 1]
+        geno_chunk = geno[start:stop, :, :].compute()
+        lanc_chunk = lanc[start:stop, :, :].compute()
+
+        for anc_i in range(n_anc):
+            # mask SNPs with local ancestry not `i_anc`
+            af[start:stop, anc_i] = (
+                np.ma.masked_where(lanc_chunk != anc_i, geno_chunk)
+                .sum(axis=(1, 2))
+                .data
+            ) / np.sum(lanc_chunk == anc_i, axis=(1, 2))
+
+    return af
+
+
+def allele_per_anc(geno, lanc, center=False, n_anc=2):
+    """Get allele count per ancestry
+
+    Parameters
+    ----------
+    ds: xr.Dataset
+        Containing geno, lanc, n_anc
+    center: bool
+        whether to center the data around empirical frequencies of each ancestry
+    inplace: bool
+        whether to return a new dataset or modify the input dataset
+    Returns
+    -------
+    Return allele counts per ancestries
+    """
+    assert np.all(geno.shape == lanc.shape), "shape of `hap` and `lanc` are not equal"
+    assert geno.ndim == 3, "`hap` and `lanc` should have three dimension"
+    n_snp, n_indiv, n_haplo = geno.shape
+    assert n_haplo == 2, "`n_haplo` should equal to 2, check your data"
+    assert center is False, "`center` is not implemented"
+    assert isinstance(geno, da.Array) & isinstance(
+        lanc, da.Array
+    ), "`geno` and `lanc` should be dask array"
+
+    # make sure the chunk size along the ploidy axis to be 2
+    geno = geno.rechunk({2: 2})
+    lanc = lanc.rechunk({2: 2})
+
+    # rechunk so that all chunk of `n_anc` is passed into the helper function
+    assert (
+        n_anc == 2
+    ), "`n_anc` should be 2, NOTE: not so clear what happens when `n_anc = 3`"
+
+    assert (
+        geno.chunks == lanc.chunks
+    ), "`geno` and `lanc` should have the same chunk size"
+
+    assert (
+        len(geno.chunks[1]) == 1
+    ), "geno / lanc should not be chunked across individual dimension"
+
+    def helper(geno_chunk, lanc_chunk, n_anc, center):
+
+        n_snp, n_indiv, n_haplo = geno_chunk.shape
+        if center:
+            af_chunk = af_per_anc(
+                da.from_array(geno_chunk), da.from_array(lanc_chunk), n_anc=n_anc
+            )
+        else:
+            af_chunk = None
+        apa = np.zeros((n_snp, n_indiv, n_anc), dtype=np.float64)
+        for i_haplo in range(n_haplo):
+            haplo_hap = geno_chunk[:, :, i_haplo]
+            haplo_lanc = lanc_chunk[:, :, i_haplo]
+            for i_anc in range(n_anc):
+                if af_chunk is None:
+                    apa[:, :, i_anc][haplo_lanc == i_anc] += haplo_hap[
+                        haplo_lanc == i_anc
+                    ]
+                else:
+                    # for each SNP, find the corresponding allele frequency
+                    apa[:, :, i_anc][haplo_lanc == i_anc] += (
+                        haplo_hap[haplo_lanc == i_anc]
+                        - af_chunk[np.where(haplo_lanc == i_anc)[0], i_anc]
+                    )
+        return apa
+
+    rls_allele_per_anc = da.map_blocks(
+        lambda geno_chunk, lanc_chunk: helper(
+            geno_chunk=geno_chunk, lanc_chunk=lanc_chunk, n_anc=n_anc, center=center
+        ),
+        geno,
+        lanc,
+        dtype=np.float64,
+    )
+
+    return rls_allele_per_anc
+
+
+def simulate_quant_pheno(
+    geno,
+    lanc,
+    n_anc=2,
+    hsq: float = None,
+    cor: float = None,
+    n_causal: int = None,
+    beta: np.ndarray = None,
+    snp_prior_var: np.ndarray = None,
+    n_sim=10,
+) -> dict:
+    """Simulate continuous phenotype of admixed individuals [continuous]
+
+    Parameters
+    ----------
+    dset: xr.Dataset
+        Dataset containing the following variables:
+            - geno: (n_indiv, n_snp, 2) phased genotype of each individual
+            - lanc: (n_indiv, n_snp, 2) local ancestry of each SNP
+    hsq: float
+        Variance explained by genotypical effects
+    var_e: float
+        Variance explained by the effect of the environment
+    gamma: float
+        Correlation between the genetic effects from two ancestral backgrounds
+    n_causal: int, optional
+        number of causal variables, by default None
+    beta: np.ndarray, optional
+        causal effect of each causal variable, by default None
+    cov_cols: List[str], optional
+        list of covariates to include as covariates, by default None
+    cov_effects: List[float], optional
+        list of the effect of each covariate, by default None
+        for each simulation, the cov_effects will be the same
+    n_sim : int, optional
+        number of simulations, by default 10
+
+
+    Returns
+    -------
+    beta: np.ndarray
+        simulated effect sizes (2 * n_snp, n_sim)
+    phe_g: np.ndarray
+        simulated genetic component of phenotypes (n_indiv, n_sim)
+    phe: np.ndarray
+        simulated phenotype (n_indiv, n_sim)
+    """
+    assert n_anc == 2, "Only two-ancestry currently supported"
+    apa = allele_per_anc(geno, lanc)
+    n_snp, n_indiv = apa.shape[0:2]
+
+    # simulate effect sizes
+    if beta is None:
+        if cor is None:
+            cor = 1.0
+
+        if n_causal is None:
+            # n_causal = n_snp if `n_causal` is not specified
+            n_causal = n_snp
+
+        assert (-1 <= cor) and (
+            cor <= 1
+        ), "Correlation parameter should be between [-1, 1]"
+
+        assert n_causal <= n_snp, "n_causal must be <= n_snp"
+
+        if snp_prior_var is None:
+            snp_prior_var = np.ones(n_snp)
+
+        # if `beta` is not specified, simulate effect sizes
+        beta = np.zeros((n_snp, n_anc, n_sim))
+        for i_sim in range(n_sim):
+            cau = sorted(
+                np.random.choice(np.arange(n_snp), size=n_causal, replace=False)
+            )
+
+            i_beta = np.random.multivariate_normal(
+                mean=[0.0, 0.0],
+                cov=np.array([[1, cor], [cor, 1]]),
+                size=n_causal,
+            )
+
+            i_beta = i_beta * np.sqrt(snp_prior_var[cau])[:, None]
+
+            for i_anc in range(n_anc):
+                beta[cau, i_anc, i_sim] = i_beta[:, i_anc]
+    else:
+        assert (cor is None) and (
+            n_causal is None
+        ), "If `beta` is specified, `cor`, and `n_causal` must not be specified"
+        assert beta.shape == (n_snp, n_anc) or beta.shape == (
+            n_snp,
+            n_anc,
+            n_sim,
+        ), "`beta` must be of shape (n_snp, n_anc) or (n_snp, n_anc, n_sim)"
+        if beta.shape == (n_snp, n_anc):
+            # replicate `beta` for each simulation
+            beta = np.repeat(beta[:, :, np.newaxis], n_sim, axis=2)
+
+    pheno_g = np.zeros([n_indiv, n_sim])
+    snp_chunks = apa.chunks[0]
+    indices = np.insert(np.cumsum(snp_chunks), 0, 0)
+
+    for i in tqdm(
+        range(len(indices) - 1), desc="admix_genet_cor.simulate_continuous_pheno"
+    ):
+        start, stop = indices[i], indices[i + 1]
+        apa_chunk = apa[start:stop, :, :].compute()
+        for i_anc in range(n_anc):
+            pheno_g += np.dot(apa_chunk[:, :, i_anc].T, beta[start:stop, i_anc, :])
+
+    # scale variance of pheno_g to hsq
+    std_scale = np.sqrt(hsq / np.var(pheno_g, axis=0))
+    pheno_g *= std_scale
+    beta *= std_scale
+
+    pheno_e = np.zeros(pheno_g.shape)
+    for i_sim in range(n_sim):
+        pheno_e[:, i_sim] = np.random.normal(
+            loc=0.0, scale=np.sqrt(1 - hsq), size=n_indiv
+        )
+
+    pheno = pheno_g + pheno_e
+    return {"beta": beta, "pheno_g": pheno_g, "pheno": pheno}
 
 
 # PAGE analysis
@@ -159,8 +404,6 @@ def simulate_het(apa, beta, cov):
         + np.random.normal(size=apa.shape[0])
     )
     return y
-
-
 
 
 if __name__ == "__main__":
